f9fc079402d68852dd111b161b3410ea71c27fc5
sshashmi <sshashmi@NIS-PK132s-MacBook-Pro.local>
2022-07-07 20:29:21 +0500

    data table alignment issue resolved

--- components/UI/DataTable.js
+++ components/UI/DataTable.js
@@ -10,16 +10,13 @@ import {
 import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
 import { DataGrid } from "@mui/x-data-grid";
 import Link from "next/link";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 const columns = [
   { field: "id", headerName: "ID", width: 100 },
   { field: "firstName", headerName: "First name", width: 150 },
   { field: "lastName", headerName: "Last name", width: 150 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 150,
-  },
+  { field: "age", headerName: "Age", width: 150 },
   {
     field: "fullName",
     headerName: "Full name",
@@ -30,13 +27,32 @@ const columns = [
       `${params.row.firstName || ""} ${params.row.lastName || ""}`,
   },
   {
-    field: "action",
-    headerName: "Action",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    field: "Action",
+    width: 150,
+    renderCell: (cellValues) => {
+      return (
+        <>
+          <IconButton
+            variant="contained"
+            color="primary"
+            // onClick={(event) => {
+            //   handleClick(event, cellValues);
+            // }}
+          >
+            <EditIcon />
+          </IconButton>
+          <IconButton
+            variant="contained"
+            color="primary"
+            // onClick={(event) => {
+            //   handleClick(event, cellValues);
+            // }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </>
+      );
+    },
   },
 ];
 
@@ -45,7 +61,7 @@ const rows = [
   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
   { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: 20 },
   { id: 6, lastName: "Melisandre", firstName: "mayan", age: 150 },
   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
