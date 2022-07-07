d23da4dfe4936482084da1c6ed87f5e7096abef1
sshashmi <sshashmi@NIS-PK132s-MacBook-Pro.local>
2022-07-07 19:53:01 +0500

    img tag warning issue resolved

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -9,17 +9,20 @@ import {
 import classes from "./login.module.css";
 import Footer from "../../components/UI/Footer";
 import Header from "../../components/UI/Header";
+// import Image from "next/image";
 const Login = () => {
   return (
     <>
       <Header />
       <Grid container>
         <Grid item xs={6}>
-          <img
-            className={classes.loginImg}
-            src="https://images.unsplash.com/photo-1452251889946-8ff5ea7b27ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fGZvb2QlMjBhcHB8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-            alt="login-img"
-          />
+          <picture>
+            <img
+              className={classes.loginImg}
+              src="https://images.unsplash.com/photo-1452251889946-8ff5ea7b27ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fGZvb2QlMjBhcHB8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+              alt="login-img"
+            />
+          </picture>
         </Grid>
         <Grid item xs={6}>
           <Container className={classes.tableStyle}>
