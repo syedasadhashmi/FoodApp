0ce63fec157f390a1b37d087498a4069e6b5e928
sshashmi <sshashmi@NIS-PK132s-MacBook-Pro.local>
2022-07-14 20:57:29 +0500

    files updated

--- pages/Orders/index.js
+++ pages/Orders/index.js
@@ -22,7 +22,26 @@ import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import Container from "@mui/material/Container";
 import classes from "./Orders.module.css";
-
+const inProgressObj = [
+  {
+    id: 1,
+    title: "Manchorian",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Doloremque natus dolore magni deserunt ad amet enim ut qui quis accusamus.",
+  },
+  {
+    id: 2,
+    title: "Manchao Soup",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Doloremque natus dolore magni deserunt ad amet enim ut qui quis accusamus.",
+  },
+  {
+    id: 3,
+    title: "Borek",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Doloremque natus dolore magni deserunt ad amet enim ut qui quis accusamus.",
+  },
+];
 const Orders = () => {
   const [value, setValue] = useState("1");
   const [expanded, setExpanded] = useState("false");
@@ -49,6 +68,30 @@ const Orders = () => {
                 </TabList>
               </Box>
               <TabPanel value="1">
+                {/* {inProgressObj.map((items) => (
+                  <Accordion
+                    key={items.id}
+                    expanded={expanded === `panel${items.id}`}
+                    onChange={(event, isExpanded) =>
+                      handleAccordianChange(isExpanded, `pannel${items.id}`)
+                    }
+                  >
+                    <AccordionSummary
+                      id={`panel${items.id}-header`}
+                      aria-control={`panel${items.id}-content`}
+                      expandIcon={<ExpandMoreIcon />}
+                    >
+                      <Typography>{items.title}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Divider />
+                      <Typography padding={2}>{items.description}</Typography>
+                      <div className={classes.inProgressBtn}>
+                        <Button variant="outlined">In Progress</Button>
+                      </div>
+                    </AccordionDetails>
+                  </Accordion>
+                ))} */}
                 <Accordion
                   expanded={expanded === "panel1"}
                   onChange={(event, isExpanded) =>
