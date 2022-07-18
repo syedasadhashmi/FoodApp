16b53cd5c3a3e4803aec08e661c0f71e7c03fb70
sshashmi <sshashmi@NIS-PK132s-MacBook-Pro.local>
2022-07-18 19:00:23 +0500

    menu form validated

--- components/UI/MenuForm.js
+++ components/UI/MenuForm.js
@@ -11,6 +11,8 @@ import {
   TextField,
 } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
+import Link from "next/link";
+import PopUp from "./PopUp";
 const DishTypeObj = [
   {
     value: "Chinese",
@@ -25,6 +27,11 @@ const DishTypeObj = [
 const MenuForm = () => {
   const [dishType, setDishType] = useState("Chinese");
   const [value, setValue] = useState("");
+  const [dishName, setDishName] = useState("");
+  const [dishPrice, setDishPrice] = useState("");
+  const [dishId, setDishId] = useState("");
+  const [restaurantName, setRestaurantName] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleMultiLineChange = (event) => {
     setValue(event.target.value);
@@ -32,86 +39,127 @@ const MenuForm = () => {
   const handleChange = (event) => {
     setDishType(event.target.value);
   };
+  const dishNameHandler = (event) => {
+    setDishName(event.target.value);
+  };
+  const dishPriceHandler = (event) => {
+    setDishPrice(event.target.value);
+  };
+  const dishIdHandler = (event) => {
+    setDishId(event.target.value);
+  };
+  const restaurantNameHandler = (event) => {
+    setRestaurantName(event.target.value);
+  };
+
+  const submitHandler = (e) => {
+    setIsSubmit(true);
+    e.preventDefault();
+    setRestaurantName("");
+    setDishId("");
+    setDishPrice("");
+    setDishName("");
+    setValue("");
+    setTimeout(() => {
+      setIsSubmit(false);
+    }, 1000);
+  };
   return (
     <Container sx={{ height: "85vh", marginTop: "20px", marginBottom: "20px" }}>
       <Card>
-        <CardHeader title="Add Menu" />
-        <Divider />
-        <CardContent>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6} md={6}>
-              <TextField
-                label="Dish Name"
-                required
-                variant="outlined"
-                fullWidth
-              />
+        <form onSubmit={submitHandler}>
+          <CardHeader title="Add Menu" />
+          <Divider />
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6} md={6}>
+                <TextField
+                  label="Dish Name"
+                  required
+                  value={dishName}
+                  onChange={dishNameHandler}
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={6}>
+                <TextField
+                  id="outlined-select-DishType"
+                  select
+                  label="Select"
+                  value={dishType}
+                  onChange={handleChange}
+                  helperText="Please select Dish Type"
+                  fullWidth
+                >
+                  {DishTypeObj.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.value}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <TextField
+                  label="Dish Price"
+                  required
+                  value={dishPrice}
+                  onChange={dishPriceHandler}
+                  variant="outlined"
+                  type="number"
+                  fullWidth
+                  helperText="Enter dish price"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <TextField
+                  label="Dish Id"
+                  required
+                  variant="outlined"
+                  value={dishId}
+                  onChange={dishIdHandler}
+                  fullWidth
+                  type="Number"
+                  helperText="Auto Generated"
+                />
+              </Grid>
+              <Grid item xs={12} sm={12} md={4}>
+                <TextField
+                  label="Restaurant Name"
+                  required
+                  variant="outlined"
+                  value={restaurantName}
+                  onChange={restaurantNameHandler}
+                  fullWidth
+                  helperText="AutoFilled"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="outlined-multiline-flexible"
+                  label="Description"
+                  multiline
+                  required
+                  maxRows={4}
+                  value={value}
+                  fullWidth
+                  onChange={handleMultiLineChange}
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6} md={6}>
-              <TextField
-                id="outlined-select-DishType"
-                select
-                label="Select"
-                value={dishType}
-                onChange={handleChange}
-                helperText="Please select Dish Type"
-                fullWidth
-              >
-                {DishTypeObj.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.value}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <TextField
-                label="Dish Price"
-                required
-                variant="outlined"
-                type="number"
-                fullWidth
-                helperText="Enter dish price"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <TextField
-                label="Dish Id"
-                required
-                variant="outlined"
-                fullWidth
-                type="Number"
-                helperText="Auto Generated"
-              />
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-              <TextField
-                label="Restaurant Name"
-                required
-                variant="outlined"
-                fullWidth
-                helperText="AutoFilled"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                id="outlined-multiline-flexible"
-                label="Description"
-                multiline
-                maxRows={4}
-                value={value}
-                fullWidth
-                onChange={handleMultiLineChange}
-              />
-            </Grid>
-          </Grid>
-        </CardContent>
-        <Divider />
-        <CardActions sx={{ float: "right", marginRight: "10px" }}>
-          <Button variant="outlined">Cancel</Button>
-          <Button variant="outlined">Save</Button>
-        </CardActions>
+          </CardContent>
+          <Divider />
+          <CardActions sx={{ float: "right", marginRight: "10px" }}>
+            <Link href="../Menu">
+              <Button variant="outlined">Cancel</Button>
+            </Link>
+            <Button variant="outlined" type="submit">
+              Save
+            </Button>
+          </CardActions>
+        </form>
       </Card>
+      {isSubmit && <PopUp />}
     </Container>
   );
 };
